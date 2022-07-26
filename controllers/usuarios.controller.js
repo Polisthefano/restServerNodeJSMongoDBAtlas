a631be04028e43c1d67a3c154fe0ee7c36461ed5
sthefano <spolitino@popapp.io>
2022-07-26 20:43:42 -0300

    agregando middlewares personalizados

--- controllers/usuarios.controller.js
+++ controllers/usuarios.controller.js
@@ -1,6 +1,8 @@
 const { response, request } = require('express') //al importar esto y poner res=response y req=request hace que vscode 
+
 const bcrypt=require('bcryptjs')
 const Usuario=require('../models/usuario.model')
+
 const usuariosGet = (req = request, res = response) => {
     const {nombre='Sin nombre',apikey}=req.query //aca vienen los query params, son los que mandamos en la url con ? el primero y despuse & para concatenar
     res.status(200).json({ //status para retornar el codigo, el 200 si no mandas nada es el default
@@ -9,10 +11,18 @@ const usuariosGet = (req = request, res = response) => {
     })
 }
 const usuariosPost = async (req, res = response) => {
+   
     const {nombre,correo,password,rol}=req.body 
     const usuario=new Usuario({nombre,correo,password,rol}) //este usuario es un objeto creado por mongoose pero es un objeto y podemos acceder a sus propiedades sin problema
+    const existeMail=await Usuario.findOne({correo}) //esto busco solo un registro
+    if(existeMail){
+        return res.status(400).json({msg:'Este email ya esta registrado'})
+    }
+    //genera hash contrasena
     const hashPassword=bcrypt.genSaltSync() //numero de vueltas para hacer mas dificil el metodo de encriptacion
     usuario.password=bcrypt.hashSync(password,hashPassword) //genera un hash de la password
+    
+    //guarda en mongo
     await usuario.save() //esto guarda el usuario creado en la collecion que se creo por medio de new Usuario()
     res.status(200).json({
      msg:'ok',
--- routes/usuario.routes.js
+++ routes/usuario.routes.js
@@ -1,10 +1,17 @@
 const { Router } = require('express')
+const { check } = require('express-validator')
 const { usuariosGet, usuariosPut, usuariosPost, usuariosDelete, usuariosPatch } = require('../controllers/usuarios.controller')
-
+const {loguearRequest}=require('../middlewares/utils')
+const validarCampos=require('../middlewares/validar-campos')
 const router = Router()
 router.get('', usuariosGet)
 router.put('/:id', usuariosPut) //forma de pasar parametros por la url
-router.post('', usuariosPost)
+router.post('', [
+    check('nombre','El nombre es requerido').not().isEmpty(), //not niega toda condicion que pongan despues
+    check('password','El password debe contener mas de 6 caracteres').isLength({min:6}), //contrasena min length
+    check('correo','El correo no es valido').isEmail(), //debe ser email
+    check('rol','No es un rol válido').isIn(['USER_ROLE','ADMIN_ROLE']), //si existe en ese array
+    loguearRequest,validarCampos],usuariosPost) //en el medio entre el path y el controllador van el array de middleware o funciones que se ejecutan antes del controlador
 router.delete('', usuariosDelete)
 router.patch('', usuariosPatch)
 module.exports = router;
