c9945833eff15895b85f0749b90c06155294ada3
sthefano <spolitino@popapp.io>
2022-08-20 19:53:42 +0200

    agregando validadores personalizados contra la db

--- database/db-validators.js
+++ database/db-validators.js
@@ -1,8 +1,15 @@
 const Role=require('../models/role.model')
-const roleDbValidator=async (rol='')=>{ //los check custom permite hacer funciones personalizadas, usan un callback en este caso busco en los roles que estan en db
+const usuarioModel = require('../models/usuario.model')
+const roleDBValidator=async (rol='')=>{ //los check custom permite hacer funciones personalizadas, usan un callback en este caso busco en los roles que estan en db
     const existeRol=await Role.findOne({rol}) //busca el documento cuyo rol sea igual
     if(!existeRol){
         throw new Error(`El rol ${rol} no es correcto`) //asi funciona express para retornar errores personalizados
     }
 }
-module.exports={roleDbValidator}
+const emailDBValidador=async(email='')=>{
+    const existeEmail= await usuarioModel.findOne({correo:email})
+    if(existeEmail){
+        throw new Error('Ese email '+email +' ya se encuentra registrado')
+            }
+}
+module.exports={roleDBValidator,emailDBValidador}
--- routes/usuario.routes.js
+++ routes/usuario.routes.js
@@ -4,7 +4,7 @@ const { usuariosGet, usuariosPut, usuariosPost, usuariosDelete, usuariosPatch } 
 
 const {loguearRequest}=require('../middlewares/utils')
 const validarCampos=require('../middlewares/validar-campos')
-const { roleDbValidator } = require('../database/db-validators') //callback de validacion personalizada del rol
+const { roleDBValidator, emailDBValidador } = require('../database/db-validators') //callback de validacion personalizada del rol
 
 const router = Router()
 router.get('', usuariosGet)
@@ -14,7 +14,8 @@ router.post('', [
     check('nombre','El nombre es requerido').not().isEmpty(), //not niega toda condicion que pongan despues
     check('password','El password debe contener mas de 6 caracteres').isLength({min:6}), //contrasena min length
     check('correo','El correo no es valido').isEmail(), //debe ser email
-    check('rol').custom(roleDbValidator),
+    check('correo').custom(emailDBValidador),
+    check('rol').custom(roleDBValidator),
     // check('rol','No es un rol válido').isIn(['USER_ROLE','ADMIN_ROLE']), //si existe en ese array
     loguearRequest,validarCampos],usuariosPost) //en el medio entre el path y el controllador van el array de middleware o funciones que se ejecutan antes del controlador
     
