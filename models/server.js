283efe16da23edee9536a1e07db175f926c46008
Sthefano <sthefanopoli@gmail>
2022-10-18 10:41:02 -0300

    probando el user email del proyecto

--- models/server.js
+++ models/server.js
@@ -1,42 +1,42 @@
-const express = require('express')
-const cors = require('cors')
-const { dbConnection } = require('../database/config')
-class Server { //server orientado a objetos
+const express = require("express");
+const cors = require("cors");
+const { dbConnection } = require("../database/config");
+class Server {
+  //server orientado a objetos
 
-    constructor() {
-        this.app = express()
-        this.port = process.env.PORT
-        this.usuariosRoutePath = '/api/usuarios'
-        this.authPath = '/api/auth'
-        //
-        //conexion a la base de datos
-          this.conectarDB()  
-        //middlewares
-        this.middlewares();
-        //rutas
-        this.routes()
-    }
+  constructor() {
+    this.app = express();
+    this.port = process.env.PORT;
+    this.usuariosRoutePath = "/api/usuarios";
+    this.authPath = "/api/auth";
+    //conexion a la base de datos
+    this.conectarDB();
+    //middlewares
+    this.middlewares();
+    //rutas
+    this.routes();
+  }
 
-    async conectarDB(){
-        await dbConnection()
-    }
-    middlewares() {
-        this.app.use(cors())
+  async conectarDB() {
+    await dbConnection();
+  }
+  middlewares() {
+    this.app.use(cors());
 
-        //lectura y parseo del body
-        this.app.use(express.json())
-            //directorio publico
-        this.app.use(express.static('public')) //use dice que vamos a usar un middleware
-            //lo corre en la ruta / path vacio
-    }
+    //lectura y parseo del body
+    this.app.use(express.json());
+    //directorio publico
+    this.app.use(express.static("public")); //use dice que vamos a usar un middleware
+    //lo corre en la ruta / path vacio
+  }
 
-    routes() {
-        this.app.use(this.usuariosRoutePath, require('../routes/usuario.routes')) //middleware de tipo route donde va la ruta y la 
-            //llamada al archivo de rutas
-        this.app.use(this.authPath, require('../routes/auth.routes'))
-    }
-    listen() {
-        this.app.listen(this.port)
-    }
+  routes() {
+    this.app.use(this.usuariosRoutePath, require("../routes/usuario.routes")); //middleware de tipo route donde va la ruta y la
+    //llamada al archivo de rutas
+    this.app.use(this.authPath, require("../routes/auth.routes"));
+  }
+  listen() {
+    this.app.listen(this.port);
+  }
 }
 module.exports = Server;
