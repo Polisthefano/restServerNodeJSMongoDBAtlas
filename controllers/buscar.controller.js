933caa6351d5b403b9684febb8edfa58f264ac89
Sthefano <sthefanopoli@gmail>
2022-12-05 12:53:14 -0300

    endpoints search in collections

--- controllers/buscar.controller.js
+++ controllers/buscar.controller.js
@@ -2,28 +2,65 @@ const { response } = require("express");
 const { isValidObjectId } = require("mongoose");
 const { Categoria, Producto, Usuario } = require("../models");
 
-const buscarUsuarios = async (termino = "", res = response) => {
+const buscarEnCollecion = async (
+  termino = "",
+  coleccion = "",
+  query = {},
+  populate = null
+) => {
   const isMongoID = isValidObjectId(termino); //Devuelve true si es mongoID valido
   if (isMongoID) {
-    const usuario = await Usuario.findById(termino);
-    return usuario;
+    const result = await coleccion.findById(termino);
+    return result ? [result] : [];
   }
+
+  const results = !populate
+    ? await coleccion.find(query)
+    : await coleccion
+        .find(query)
+        .populate(populate.collection, populate.fields);
+  return results;
 };
 
 const buscar = async (req, res = response) => {
   const { collecion, termino } = req.params;
-  const response = { results: [] };
+  const regExp = new RegExp(termino, "i"); //hace una expresion regular donde le dice por medio de 'i' que es insensible a mayusculas y minusculas
+  const response = { results: [], totalResults: 0 };
   switch (collecion) {
     case "categorias":
+      const categorias = await buscarEnCollecion(
+        termino,
+        Categoria,
+        {
+          $or: [{ nombre: regExp, estado: true }],
+        },
+        { collection: "usuario", fields: ["nombre", "id"] }
+      );
+      response.results = categorias;
       break;
     case "usuarios":
-      const usuarios = await buscarUsuarios(termino);
-      response.results = usuarios ? [usuarios] : [];
-      return res.status(200).json(response);
+      const usuarios = await buscarEnCollecion(termino, Usuario, {
+        $or: [{ nombre: regExp }, { correo: regExp }], //de esta forma mediante mongoose, y con el $or decimos que busque nombre o correo
+        $and: [{ estado: true }], //y que cumpla eso
+      });
+      response.results = usuarios;
       break;
     case "productos":
+      const productos = await buscarEnCollecion(
+        termino,
+        Producto,
+        {
+          $or: [{ nombre: regExp, estado: true }],
+        },
+        { collection: "categoria", fields: ["nombre", "id"] }
+      );
+      response.results = productos;
+      break;
     default:
       res.status(500).json("No desarrollada la busqueda para esta collecion");
   }
+
+  response.totalResults = response.results.length;
+  return res.status(200).json(response);
 };
 module.exports = { buscar };
